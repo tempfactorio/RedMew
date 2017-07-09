c28d66de86181136af0f3e61240235952ba020b4
Valansch <mail@maikwild.de>
2017-07-09 13:29:30 +0200

    changed items on join

--- control.lua
+++ control.lua
@@ -22,15 +22,11 @@ function player_joined(event)
 		player.insert { name = "raw-fish", count = 4 }
 	    player.insert { name = "iron-gear-wheel", count = 8 }
 	    player.insert { name = "iron-plate", count = 16 }
-	    --player.insert { name = "pistol", count = 1 }
-	    --player.insert { name = "firearm-magazine", count = 8 }
-		--player.insert { name = "train-stop", count = 16 }
-		--player.insert { name = "roboport", count = 16 }
-		--player.insert { name = "construction-robot", count = 16 }
-		--player.insert { name = "solar-panel", count = 16 }
-		--player.insert { name = "substation", count = 16 }
-		--player.insert { name = "logistic-chest-passive-provider", count = 16 }
-		--player.insert { name = "power-armor", count = 1 }
+	    player.insert { name = "steel-axe", count = 1 }
+	    player.insert { name = "pistol", count = 1 }
+			player.insert { name = "firearm-magazine", count = 10 }
+	    player.insert { name = "burner-mining-drill", count = 1 }
+			player.insert { name = "stone-furnace", count = 1 }
 		player.print("Welcome to our Server. You can join our Discord at: discord.me/redmew")
 		player.print("And remember.. Keep Calm And Spaghetti!")
 end
